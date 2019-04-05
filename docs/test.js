ee4e777d55085a27c9cba985aff587fb291d8666
ReeceM <reecestevenmay@gmail.com>
2019-04-05 22:44:24 +0200

    fix: push to plugins, not overwrite....

--- docs/test.js
+++ docs/test.js
@@ -6,26 +6,25 @@
         __digesterjs = module.default
     })
 
-    window.$docsify.plugins = [
-        function (hook, vm) {
-            hook.doneEach(() => {
-                document.getElementById('test-button').onclick = () => {
-                    let date = new Date().toLocaleString()
-                    let result_element = document.getElementById('result')
+    var plugin = function (hook, vm) {
+        hook.doneEach(() => {
+            document.getElementById('test-button').onclick = () => {
+                let date = new Date().toLocaleString()
+                let result_element = document.getElementById('result')
 
-                    __digesterjs.__sha1Hash(date, (result, err = null) => {
+                __digesterjs.__sha1Hash(date, (result, err = null) => {
 
-                        if (err) {
-                            console.error('[ERROR - digesterjs] %o', err)
-                            result_element.innerText = `[ERROR - digesterjs] ${err}`
-                        }
+                    if (err) {
+                        console.error('[ERROR - digesterjs] %o', err)
+                        result_element.innerText = `[ERROR - digesterjs] ${err}`
+                    }
 
-                        result_element.innerText = `SHA-1: ${result} --- Time: ${date}`
+                    result_element.innerText = `SHA-1: ${result} --- Time: ${date}`
 
-                    })
-                }
-            })
-        }
-    ]
-
+                })
+            }
+        })
+    }
+        
+    window.$docsify.plugins.push(plugin)
 })();
